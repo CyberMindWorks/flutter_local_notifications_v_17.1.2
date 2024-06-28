071a7ea7ca1f24eb2a3074933f0d37e8f9cdf5c8
Leo-cmw <leo@cybermindworks.com>
2024-06-28 18:22:29 +0530

    added import for refer 'R' package

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
@@ -102,6 +102,7 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry;
+import com.dexterous.flutterlocalnotifications.R;
 
 interface PermissionRequestListener {
   void complete(boolean granted);
